59f16e7607d53e061983b792b9c3dd6eb5e8f49a
PedroKahane <pedrok@datosclaros.com>
2021-06-24 10:33:57 -0300

    actualizaciones varias

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.listen(process.env.PORT || 3030,function() {
-     console.log("Server on http://localhost:3030");
+     console.log("Servidor corriendo en puerto 3030");
 })
 
 
